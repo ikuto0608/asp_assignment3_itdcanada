8e9d413ffd9a08720b116805d5291f9d3b620951
ikuto0608 <ikuto0608@me.com>
2016-09-21 11:48:50 -0700

    finished

--- App_Code/AdminUser.cs
+++ App_Code/AdminUser.cs
@@ -49,4 +49,38 @@ public class AdminUser
         }
         return user;
     }
+
+    public static AdminUser findByID(int id)
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+        SqlCommand command = new SqlCommand();
+        SqlDataReader reader;
+
+        AdminUser user = new AdminUser();
+        try
+        {
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "select * from users where id=" + id;
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                user.ID = Convert.ToInt32(reader.GetValue(0));
+                user.UserName = reader.GetValue(1).ToString();
+                user.Password = reader.GetValue(2).ToString();
+                user.FirstName = reader.GetValue(3).ToString();
+                user.LastName = reader.GetValue(3).ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return user;
+    }
 }
--- App_Code/Car.cs
+++ App_Code/Car.cs
@@ -76,8 +76,15 @@ public class Car
         {
             con.Open();
             command.Connection = con;
-            command.CommandText = "insert into cars (vin, make, model, year, color, mileage, total_damage_to_date, price, image_path, special_features, accidents) values ('" + this.VIN + "', '" + this.Make + "', '" + this.Model + "', '" + this.Year + "', '" + this.Color + "', " + this.Mileage + ", '" + this.TotalDamageToDate + "', " + this.Price + ", '" + this.ImagePath + "', '" + this.SpecialFeatures + "', '" + this.Accidents + "')";
-            command.ExecuteNonQuery();
+            if (this.ID > 0)
+            {
+                command.CommandText = "update cars set vin='" + this.VIN + "', make='" + this.Make + "', model='" + this.Model + "', year=" + this.Year + ", color='" + this.Color + "', mileage=" + this.Mileage + ", total_damage_to_date='" + this.TotalDamageToDate + "', price=" + this.Price + ", image_path='" + this.ImagePath + "', special_features='" + this.SpecialFeatures + "', accidents='" + this.Accidents + "' WHERE id=" + this.ID;
+            }
+            else
+            {
+                command.CommandText = "insert into cars (vin, make, model, year, color, mileage, total_damage_to_date, price, image_path, special_features, accidents) values ('" + this.VIN + "', '" + this.Make + "', '" + this.Model + "', '" + this.Year + "', '" + this.Color + "', " + this.Mileage + ", '" + this.TotalDamageToDate + "', " + this.Price + ", '" + this.ImagePath + "', '" + this.SpecialFeatures + "', '" + this.Accidents + "')";
+            }
+           command.ExecuteNonQuery();
         }
         catch (Exception ex)
         {
@@ -129,5 +136,127 @@ public class Car
             con.Close();
         }
         return car;
+    }
+
+    public static bool delete(int id)
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+
+        Car car = new Car();
+        try
+        {
+            con.Open();
+            using (SqlCommand command = new SqlCommand("delete from cars where id=" + id, con))
+            {
+                command.ExecuteNonQuery();
+            }
+            con.Close();
+
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return true;
+    }
+
+    public static List<Car> findByMulti(string make, string model, string year, string fromPrice, string toPrice)
+    {
+        List<Car> cars = new List<Car>();
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+        SqlCommand command = new SqlCommand();
+        SqlDataReader reader;
+
+        try
+        {
+            con.Open();
+            command.Connection = con;
+            Dictionary<string, string> keysDictionary = new Dictionary<string, string>();
+            if (make != "") {
+                keysDictionary.Add("make", make);
+            }
+            if (model != "") {
+                keysDictionary.Add("model", model);
+            }
+            if (year != "") {
+                keysDictionary.Add("year", year);
+            }
+            if (fromPrice != "" && toPrice != "") {
+                keysDictionary.Add("fromPrice", fromPrice);
+                keysDictionary.Add("toPrice", toPrice);
+            }
+
+            string searchStr = "";
+
+            foreach (var element in keysDictionary) {
+                if (searchStr.Length == 0) {
+                    searchStr += " where ";
+                }
+                
+                switch (element.Key)
+                {
+                    case "make":
+                    case "model":
+                        if (searchStr.Length > 7) {
+                            searchStr += " and (" + element.Key + "='" + element.Value + "')";
+                        } else {
+                            searchStr += "(" + element.Key + "='" + element.Value + "')";
+                        }
+                        break;
+                    case "year":
+                        if (searchStr.Length > 7) {
+                            searchStr += " and (" + element.Key + "=" + element.Value + ")";
+                        } else {
+                            searchStr += "(" + element.Key + "=" + element.Value + ")";
+                        }
+                        break;
+                    case "fromPrice":
+                        if (searchStr.Length > 7) {
+                            searchStr += " price between " + element.Value + " and " + keysDictionary["toPrice"];
+                        } else {
+                            searchStr += "price between " + element.Value + " and " + keysDictionary["toPrice"];
+                        }
+                        break;
+
+                }
+
+            }
+
+            command.CommandText = "select * from cars" + searchStr;
+
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Car car = new Car();
+                car.ID = Convert.ToInt32(reader.GetValue(0));
+                car.VIN = reader.GetValue(1).ToString();
+                car.Make = reader.GetValue(2).ToString();
+                car.Model = reader.GetValue(3).ToString();
+                car.Year = Convert.ToInt32(reader.GetValue(4));
+                car.Color = reader.GetValue(5).ToString();
+                car.Mileage = Convert.ToInt32(reader.GetValue(6));
+                car.Accidents = reader.GetValue(7).ToString();
+                car.TotalDamageToDate = reader.GetValue(8).ToString();
+                car.Price = Convert.ToInt32(reader.GetValue(9));
+                car.ImagePath = reader.GetValue(10).ToString();
+                car.SpecialFeatures = reader.GetValue(11).ToString();
+
+                cars.Add(car);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return cars;
     }
 }
