a1acd4c1f0a7ca280c016e6aeff7fca0237fb25b
ikuto0608 <ikuto0608@me.com>
2016-09-21 09:09:12 -0700

    first commit

---
+++ App_Code/AdminUser.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+public class AdminUser
+{
+    public AdminUser()
+	{
+	}
+
+    public int ID { get; set; }
+    public string UserName { get; set; }
+    public string Password { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public static AdminUser findByUserName(string userName)
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+        SqlCommand command = new SqlCommand();
+        SqlDataReader reader;
+
+        AdminUser user = new AdminUser();
+        try
+        {
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "select * from users where user_name='" + userName + "'";
+            reader = command.ExecuteReader();
+
+            while (reader.Read())
+            {
+                user.ID = Convert.ToInt32(reader.GetValue(0));
+                user.UserName = reader.GetValue(1).ToString();
+                user.Password = reader.GetValue(2).ToString();
+                user.FirstName = reader.GetValue(3).ToString();
+                user.LastName = reader.GetValue(3).ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return user;
+    }
+}
---
+++ App_Code/Car.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+public class Car
+{
+    public Car()
+	{
+	}
+
+    public int ID { get; set; }
+    public string VIN { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public string Color { get; set; }
+    public int Mileage { get; set; }
+    public string Accidents { get; set; }
+    public string TotalDamageToDate { get; set; }
+    public int Price { get; set; }
+    public string ImagePath { get; set; }
+    public string SpecialFeatures { get; set; }
+
+    public static List<Car> all()
+    {
+        List<Car> cars = new List<Car>();
+
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+        SqlCommand command = new SqlCommand();
+        SqlDataReader reader;
+
+        try
+        {
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "select * from cars";
+            reader = command.ExecuteReader();
+            while (reader.Read())
+            {
+                Car car = new Car();
+                car.ID = Convert.ToInt32(reader.GetValue(0));
+                car.VIN = reader.GetValue(1).ToString();
+                car.Make = reader.GetValue(2).ToString();
+                car.Model = reader.GetValue(3).ToString();
+                car.Year = Convert.ToInt32(reader.GetValue(4));
+                car.Color = reader.GetValue(5).ToString();
+                car.Mileage = Convert.ToInt32(reader.GetValue(6));
+                car.Accidents = reader.GetValue(7).ToString();
+                car.TotalDamageToDate = reader.GetValue(8).ToString();
+                car.Price = Convert.ToInt32(reader.GetValue(9));
+                car.ImagePath = reader.GetValue(10).ToString();
+                car.SpecialFeatures = reader.GetValue(11).ToString();
+
+                cars.Add(car);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return cars;
+    }
+
+    public bool save()
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+        SqlCommand command = new SqlCommand();
+
+        try
+        {
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "insert into cars (vin, make, model, year, color, mileage, total_damage_to_date, price, image_path, special_features, accidents) values ('" + this.VIN + "', '" + this.Make + "', '" + this.Model + "', '" + this.Year + "', '" + this.Color + "', " + this.Mileage + ", '" + this.TotalDamageToDate + "', " + this.Price + ", '" + this.ImagePath + "', '" + this.SpecialFeatures + "', '" + this.Accidents + "')";
+            command.ExecuteNonQuery();
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return true;
+    }
+
+    public static Car findByID(int id)
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=AspAssignment3;Integrated Security=True;");
+        SqlCommand command = new SqlCommand();
+        SqlDataReader reader;
+
+        Car car = new Car();
+        try
+        {
+            con.Open();
+            command.Connection = con;
+            command.CommandText = "select * from cars where id=" + id;
+            reader = command.ExecuteReader();
+            
+            while (reader.Read())
+            {
+                car.ID = Convert.ToInt32(reader.GetValue(0));
+                car.VIN = reader.GetValue(1).ToString();
+                car.Make = reader.GetValue(2).ToString();
+                car.Model = reader.GetValue(3).ToString();
+                car.Year = Convert.ToInt32(reader.GetValue(4));
+                car.Color = reader.GetValue(5).ToString();
+                car.Mileage = Convert.ToInt32(reader.GetValue(6));
+                car.Accidents = reader.GetValue(7).ToString();
+                car.TotalDamageToDate = reader.GetValue(8).ToString();
+                car.Price = Convert.ToInt32(reader.GetValue(9));
+                car.ImagePath = reader.GetValue(10).ToString();
+                car.SpecialFeatures = reader.GetValue(11).ToString();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.Write(ex);
+        }
+        finally
+        {
+            con.Close();
+        }
+        return car;
+    }
+}
